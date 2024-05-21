3f017672e3dc5790265bc5e3a6309efbf7ef9511
Khushal Mali <khushalmali8448@gmail.com>
2024-05-21 14:08:38 +0530

    folder structure

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,7 @@
+import React, { type ReactNode } from "react";
+
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return <main>{children}</main>;
+};
+
+export default RootLayout;
