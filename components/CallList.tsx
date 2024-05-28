720ee289d22dd2e8448acab17132a30984142f57
Khushal Mali <khushalmali8448@gmail.com>
2024-05-28 11:20:15 +0530

    bug fix

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -81,13 +81,13 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
                 : "/icons/recordings.svg"
             }
             title={
-              (meeting as Call).state?.custom.description.substring(0, 26) ||
-              meeting.filename.substring(0, 20) ||
-              "No description"
+              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
+              meeting?.filename?.substring(0, 20) ||
+              "Personal Meeting"
             }
             date={
-              meeting.state?.startedAt?.toLocaleString() ||
-              meeting.start_time.toLocaleString()
+              meeting.state?.startsAt?.toLocaleString() ||
+              meeting?.start_time?.toLocaleString()
             }
             isPreviousMeeting={type === "ended"}
             buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
